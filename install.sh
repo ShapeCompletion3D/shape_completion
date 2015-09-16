d29f96e37642f0791c27d0a856e7ec746c6f6135
Avinash Nair <asn2129@columbia.edu>
2015-09-16 11:51:21 -0400

    Updated installation script for 3D Shape Completion

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # Installation Script for 3D Shape Completion
 
-echo "Hello World!"
+git clone https://github.com/3DPylearnConv/shape_completion_experiments.git
+git clone https://github.com/3DPylearnConv/keras.git
+git clone https://github.com/3DPylearnConv/binvox-rw-py.git
