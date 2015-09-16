58fd8c4e11e6dc8313a63dd1c985d251375a321e
Avinash Nair <asn2129@columbia.edu>
2015-09-16 17:25:08 -0400

    Added a set_paths.sh file to set environment variables

---
+++ set_paths.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Setting paths for Shape Completion Libraries
+
+export HOMEPATH=$PWD
+export PYTHONPATH=$HOMEPATH/shape_completion_experiments:$HOMEPATH/binvox-rw-py:$HOMEPATH/keras:$PYTHONPATH
