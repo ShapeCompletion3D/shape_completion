02fd0c66438eac720359112c6863ff23f2a1e218
avinair <asn2129@columbia.edu>
2015-10-07 13:49:12 -0400

    Modified installation script to install pymcubes and pycollada

--- install.sh
+++ install.sh
@@ -4,3 +4,13 @@
 git clone https://github.com/3DPylearnConv/shape_completion_experiments.git
 git clone https://github.com/3DPylearnConv/keras.git
 git clone https://github.com/3DPylearnConv/binvox-rw-py.git
+git clone https://github.com/pycollada/pycollada.git
+cd pycollada
+python setup.py build
+python setup.py install --user
+cd ..
+git clone https://github.com/pmneila/PyMCubes.git
+cd PyMCubes
+python setup.py build
+python setup.py install --user
+cd ..
