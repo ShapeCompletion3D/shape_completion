2fe98d4f008a8cf10ab87adc85b3b3adf228f5e2
avinair <asn2129@columbia.edu>
2015-12-02 15:27:32 -0500

    Added pcl to the python path

--- set_paths.sh
+++ set_paths.sh
@@ -2,4 +2,4 @@
 # Setting paths for Shape Completion Libraries
 
 export HOMEPATH=$PWD
-export PYTHONPATH=$HOMEPATH/train/shape_completion_experiments:$HOMEPATH/utils/binvox-rw-py:$HOMEPATH/utils/keras:$HOMEPATH/utils/PyMCubes:$PYTHONPATH
+export PYTHONPATH=$HOMEPATH/train/shape_completion_experiments:$HOMEPATH/utils/binvox-rw-py:$HOMEPATH/utils/keras:$HOMEPATH/utils/PyMCubes:$HOMEPATH/utils/python-pcl:$PYTHONPATH
